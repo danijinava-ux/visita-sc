e730e339243140c130e385c51e306f4a9ac21024
danijinava-ux <danijinava@gmail.com>
2025-09-23 23:44:32 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,227 @@
+const visitData = {
+    congregation: "CONGREGACION HUNTER 2063",
+    circuit: "Semana de la visita del Superintendente de Circuito",
+    superintendentName: "Hno. Julio Baca",
+    date: "30 de Septiembre al 5 de Octubre",
+    motto: "“Así que recíbanlo como suelen recibir a los seguidores del Señor, con mucha alegría. Y tengan siempre en alta estima a hombres como él” (Fil 2:29)",
+
+    reunions: [
+        {
+            day: "Martes 30 de Septiembre",
+            title: "Reunión de entre semana Vida y Ministerio Cristianos",
+            scripture: "Eclesiastés 3, 4",
+            details: [
+                "Presidente: Waldo Vera",
+                "Canción 93 y Oración",
+                "",
+                "<b>TESOROS DE LA BIBLIA</b>",
+                "1. Fortalezcan su cuerda triple (10 mins.) - Edwin Arapa",
+                "2. Busquemos perlas escondidas (10 mins.) - David Gutiérrez",
+                "3. Lectura de la Biblia (4 mins.) Ec 4:1-16 (th lección 2) - Edgar Limo",
+                "",
+                "<b>SEAMOS MEJORES MAESTROS</b>",
+                "4. Empiece conversaciones (3 mins.) (lmd lección 2 punto 5) - Alberto Acevedo / Juana Acevedo",
+                "5. Haga revisitas (4 mins.) (lmd lección 9 punto 4) - Hobab Romero / Julio Chicata",
+                "6. Discurso (5 mins.) (th lección 19) - Ramón Quispe",
+                "",
+                "<b>NUESTRA VIDA CRISTIANA</b>",
+                "7. Cuando tengan problemas en su matrimonio, no aparten a Jehová de su vida (15 mins.) - José Catacora",
+                "",
+                "<b>DISCURSO DE SERVICIO</b>",
+                "“EL AMOR UN LAZO DE UNIÓN PERFECTO” (30 mins.) - S.C. Julio Baca",
+                "Canción 109 y oración conclusión - Julio Baca"
+            ]
+        },
+        {
+            day: "Sábado 04 de Octubre",
+            title: "Reunión del fin de semana",
+            scripture: "",
+            details: [
+                "<b>DISCURSO PÚBLICO</b>",
+                "Presidente: Dennis Apaza",
+                "Canción 155 y Oración inicial",
+                "“¿QUÉ LO AYUDARÁ A TENER ÉXITO EN LA VIDA?” (30 min) - S.C JULIO BACA",
+                "",
+                "<b>ESTUDIO DE LA ATALAYA</b>",
+                "Conductor: Daniel Jimenez",
+                "Canción: 97",
+                "Tema: “¿Puede seguir aprendiendo de las enseñanzas básicas?” (preguntas y respuestas)",
+                "",
+                "<b>DISCURSO FINAL</b>",
+                "“¿LO CONDICIONAN LAS CIRCUNSTANCIAS?” (30 min) - S.C JULIO BACA",
+                "Canción: 128 y oración final"
+            ]
+        }
+    ],
+
+    fieldService: [
+        {
+            day: "Martes 30 Sept.",
+            time: "8:30 am",
+            meetingPoint: "Cancha Juventud Unida",
+            conductor: "Dennis Apaza",
+            territory: "24, 25, 26, 27, 28"
+        },
+        {
+            day: "Miércoles 01 Oct.",
+            time: "8:30 am",
+            meetingPoint: "Salón del Reino",
+            conductor: "Julio Baca",
+            territory: "29, 30, 31, 32, 38, 39, 40, 80, 81, 82, 83, 84"
+        },
+        {
+            day: "Miércoles 01 Oct.",
+            time: "3:50 pm",
+            meetingPoint: "Salón del Reino",
+            conductor: "Julio Baca",
+            territory: "33, 34, 35, 36, 37"
+        },
+        {
+            day: "Jueves 02 Oct.",
+            time: "8:30 am",
+            meetingPoint: "Salón del Reino",
+            conductor: "Julio Baca",
+            territory: "42, 43, 46, 47, 48, 49, 50"
+        },
+        {
+            day: "Jueves 02 Oct.",
+            time: "3:50 pm",
+            meetingPoint: "Salón del Reino",
+            conductor: "Julio Baca",
+            territory: "33, 34, 35, 36, 37"
+        },
+        {
+            day: "Viernes 03 Oct.",
+            time: "8:30 am",
+            meetingPoint: "Salón del Reino",
+            conductor: "Julio Baca",
+            territory: "75, 76, 77, 78, 79"
+        },
+        {
+            day: "Viernes 03 Oct.",
+            time: "3:50 pm",
+            meetingPoint: "Fam. Jiménez",
+            conductor: "Julio Baca",
+            territory: "60, 61, 63, 65, 66, 67"
+        },
+        {
+            day: "Sábado 04 Oct.",
+            time: "9:00 am",
+            meetingPoint: "Fam. Jiménez",
+            conductor: "Julio Baca",
+            territory: "68, 69, 70, 71, 72, 73, 74"
+        },
+        {
+            day: "Domingo 05 Oct.",
+            time: "9:00 am (G.1-5)",
+            meetingPoint: "Fam. Puma (Jesús y Vanesa)",
+            conductor: "Julio Baca",
+            territory: "52, 53, 54, 55, 56, 57, 58"
+        }
+    ],
+
+    studiesAndRevisits: [
+        {
+            day: "Miércoles",
+            time: "4:00 pm",
+            brother: "Julio Chicata",
+            sister: "Consuelo Romero"
+        },
+        {
+            day: "Miércoles",
+            time: "5:00 pm",
+            brother: "Delfina Mamani",
+            sister: "Patricia Neyra"
+        },
+        {
+            day: "Jueves",
+            time: "Mañana",
+            brother: "Dolores Pari",
+            sister: "Nelly Cuba"
+        },
+        {
+            day: "Jueves",
+            time: "Mañana",
+            brother: "",
+            sister: "Lidia Cabrera"
+        },
+        {
+            day: "Viernes",
+            time: "4:00 pm",
+            brother: "Rodrigo Mamani",
+            sister: "Ivette Patiño"
+        },
+        {
+            day: "Viernes",
+            time: "5:00 pm",
+            brother: "Lucia Mamani",
+            sister: "Jenny Jimenez"
+        }
+    ],
+
+    hospitality: [
+        {
+            day: "MARTES",
+            breakfast: "Fam. Idme",
+            lunch: "Lidia Cabrera",
+            dinner: "Celestino Condori"
+        },
+        {
+            day: "MIÉRCOLES",
+            breakfast: "Rosario Flores",
+            lunch: "Joel Aragón",
+            dinner: "Julio Chicata"
+        },
+        {
+            day: "JUEVES",
+            breakfast: "Dennis Apaza",
+            lunch: "Fam. Jiménez",
+            dinner: "Fam. Arapa"
+        },
+        {
+            day: "VIERNES",
+            breakfast: "Maria del C. Gallegos",
+            lunch: "Ivette Patiño",
+            dinner: "Fam. Kcana"
+        },
+        {
+            day: "SÁBADO",
+            breakfast: "Beatriz Salas",
+            lunch: "Rodrigo Mamani",
+            dinner: "Elizabeth Ochoa"
+        },
+        {
+            day: "DOMINGO",
+            breakfast: "Fam. Pacco",
+            lunch: "Fam. Romero",
+            dinner: "Rocio Arapa"
+        }
+    ],
+
+    additionalMeetings: [
+        {
+            day: "MARTES",
+            time: "4:00 p. m.",
+            reunion: "REVISIÓN DE ARCHIVOS",
+            place: "COORDINAR"
+        },
+        {
+            day: "JUEVES",
+            time: "6:30 p. m.",
+            reunion: "REUNIÓN DE PRECURSORES",
+            place: "SALÓN DEL REINO"
+        },
+        {
+            day: "VIERNES",
+            time: "6:00 p. m.",
+            reunion: "REUN. DE ANC. Y SIERVOS M.",
+            place: "SALÓN DEL REINO"
+        }
+    ],
+
+    reminders: [
+        "1. Llegar temprano a las reuniones.",
+        "2. En el estudio de La Atalaya, no habrá lectura, la consideración solo será por preguntas y respuestas.",
+        "3. Agradecemos las muestras de hospitalidad. Solo como recomendación, a quienes darán los almuerzos, tenerlos a las 11:30 am."
+    ]
+};
